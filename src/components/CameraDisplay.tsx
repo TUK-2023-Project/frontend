8d7cfadcae556867300df32e55d3aa521271478f
teawon <xodnjsvkvk9@gmail.com>
2023-02-05 17:35:20 +0900

    feat : 카메라 on/off 기능 추가

--- src/components/CameraDisplay.tsx
+++ src/components/CameraDisplay.tsx
@@ -1,26 +1,46 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
+import classNames from "classnames";
+import "./CameraDisplay.scss";
 
 const CameraDisplay: React.FC = () => {
   const videoRef = useRef<HTMLVideoElement>(null);
+  const [stream, setStream] = useState<MediaStream | null>(null);
 
-  const handleClick = async () => {
-    try {
-      const stream = await navigator.mediaDevices.getUserMedia({
-        video: true,
+  const handleStream = async () => {
+    if (stream != null) {
+      stream.getTracks().forEach((track) => {
+        track.stop();
       });
-      if (videoRef.current != null) {
-        videoRef.current.srcObject = stream;
+      setStream(null);
+    } else {
+      try {
+        const newStream = await navigator.mediaDevices.getUserMedia({
+          video: true,
+        });
+        setStream(newStream);
+        if (videoRef.current != null) {
+          videoRef.current.srcObject = newStream;
+        }
+      } catch (error) {
+        console.error(error);
       }
-    } catch (error) {
-      console.error(error);
     }
   };
-
   return (
     <div>
-      <video ref={videoRef} autoPlay />
-      <button onClick={handleClick}>영상 시작</button>
+      <div>
+        <button onClick={handleStream}>
+          {stream == null
+            ? "영상 시작(퀴즈 진행 중 보여줄 때 가정 - 임시버튼)"
+            : "영상 종료(퀴즈 진행 중 보여주지 않을 때 가정 - 임시버튼)"}
+        </button>
+      </div>
+      <video
+        className={classNames(stream != null ? "" : "hidden")}
+        ref={videoRef}
+        autoPlay
+      />
     </div>
   );
 };
