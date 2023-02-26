e0eb073422b67b3a16c4f3c76dcf8c27fdc8ff6a
minjilee <budlalswl1129@gmail.com>
2023-02-27 02:04:54 +0900

    test: 프론트 웹소켓 테스트 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import WebSocketText from "pages/WebSocketText";
 import React from "react";
-// import { Route, Routes } from "react-router-dom";
-// import PlaygroudPage from "./pages/PlaygroudPage";
+import { Route, Routes } from "react-router-dom";
+import PlaygroudPage from "./pages/PlaygroudPage";
 
 function App() {
-  return <WebSocketText />;
+  return (
+    <Routes>
+      <Route path={"/playground"} element={<PlaygroudPage />} />
+      <Route path={"/"} element={<PlaygroudPage />} />
+    </Routes>
+  );
 }
 
 export default App;
