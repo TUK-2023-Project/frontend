596a9a735d51b5196018fb44289e983db77345b9
teawon <xodnjsvkvk9@gmail.com>
2023-03-11 17:34:53 +0900

    feat : 테스트용 Action, Reducer 생성

--- src/redux/configStore.ts
+++ src/redux/configStore.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { TaskReducer } from "./reducers/TaskReducer";
+import { TestReducer } from "./reducers/TestReducer";
 
 const store = configureStore({
   reducer: {
     Task: TaskReducer,
+    Test: TestReducer,
   },
   devTools: process.env.NODE_ENV !== "production",
 });
