fba64ad64045e0f9321e941636853a3f82b2df38
teawon <xodnjsvkvk9@gmail.com>
2023-03-18 17:04:14 +0900

    design : 메인 페이지 css 반영

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import "index.css";
-
 import store from "./redux/configStore";
 
 const root = ReactDOM.createRoot(
