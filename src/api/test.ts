2faf1ac2f59beaf68ca32a0edf1f42373d91aff3
teawon <xodnjsvkvk9@gmail.com>
2023-04-01 16:40:01 +0900

    refactor : react-query 폴더 구조 정리

--- src/api/test.ts
+++ src/api/test.ts
@@ -1,6 +1,24 @@
-export const fetchingData = async () => {
+import { useQuery } from "@tanstack/react-query";
+import { useDispatch } from "react-redux";
+import { setNewNickname } from "../redux/actions/TestActions";
+
+const getUserName = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 1000)); // Loading 테스트
   const res = await fetch("../dummy/duplication.json");
   const json = await res.json();
   console.log(json);
   return json;
+};
+
+export const loadUserNameData = () => {
+  const dispatch = useDispatch();
+
+  const { isLoading, error, data } = useQuery(["nicknameData"], getUserName, {
+    retry: 0,
+    onSuccess: (data) => {
+      dispatch(setNewNickname(data.nicknameData));
+    },
+  });
+
+  return { isLoading, error, data };
 };
--- src/pages/Playground/index.tsx
+++ src/pages/Playground/index.tsx
@@ -1,29 +1,14 @@
-import React, { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { setNewNickname } from "redux/actions/TestActions";
+import React from "react";
+import { useSelector } from "react-redux";
 import "./PlaygroundPage.scss"; // scss 임포트
-import { useQuery } from "@tanstack/react-query";
-import { fetchingData } from "../../api/test";
+import { loadUserNameData } from "../../api/test";
 
 function PlaygroudPage() {
-  const dispatch = useDispatch();
   const nickname = useSelector(
     (state: { Test: { nickname: string } }) => state.Test.nickname
   );
-  const [name, setName] = useState(nickname);
 
-  const {
-    isLoading,
-    error,
-    data: nicknameData,
-  } = useQuery(["nicknameData"], fetchingData);
-
-  useEffect(() => {
-    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
-    if (nicknameData) {
-      dispatch(setNewNickname(nicknameData.nicknameData));
-    }
-  }, [nicknameData]);
+  const { isLoading, error } = loadUserNameData();
 
   // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
   if (isLoading) {
@@ -33,6 +18,7 @@ function PlaygroudPage() {
   if (error) {
     return <h1>error</h1>;
   }
+
   return (
     <div>
       <h1>낙서장 페이지</h1>
