3a6809ab59005448e48f76f687db1cdb90098765
minjilee <budlalswl1129@gmail.com>
2023-04-14 23:43:30 +0900

    feat: 로그인 여부에 따른 메인페이지 화면출력

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import styles from "./MainPage.module.scss";
 
 function MainPage() {
+  console.log(!(localStorage.getItem("accessToken") == null));
   return (
     <div className={styles.content}>
       <h1>
@@ -11,17 +12,25 @@ function MainPage() {
         <br />
         Sign-language-Quiz
       </h1>
-      <div className={styles["content__common-btn-wrap"]}>
-        <Link to="/game">
-          <CommonButton buttonName="시작하기" />
-        </Link>
-        <Link to="/rank">
-          <CommonButton buttonName="랭킹확인" />
-        </Link>
-        <Link to="/incorrectnote">
-          <CommonButton buttonName="오답노트" />
-        </Link>
-      </div>
+      {localStorage.getItem("accessToken") == null ? (
+        <div className={styles["content__common-btn-wrap-no"]}>
+          <Link to="/signin">
+            <CommonButton buttonName="시작하기" />
+          </Link>
+        </div>
+      ) : (
+        <div className={styles["content__common-btn-wrap-yes"]}>
+          <Link to="/game">
+            <CommonButton buttonName="시작하기" />
+          </Link>
+          <Link to="/rank">
+            <CommonButton buttonName="랭킹확인" />
+          </Link>
+          <Link to="/incorrectnote">
+            <CommonButton buttonName="오답노트" />
+          </Link>
+        </div>
+      )}
     </div>
   );
 }
