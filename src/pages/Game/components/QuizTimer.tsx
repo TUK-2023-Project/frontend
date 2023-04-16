887e0f2bd92a1ec3f710f7c6e919283860a3f7c2
teawon <xodnjsvkvk9@gmail.com>
2023-04-16 16:34:03 +0900

    design : 타이머색상 초록->빨강으로 변하도록 수정

--- src/pages/Game/components/QuizTimer.tsx
+++ src/pages/Game/components/QuizTimer.tsx
@@ -1,15 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { useTimer } from "react-timer-hook";
 import ProgressBar from "@ramonak/react-progress-bar";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { moveNextStage, timeOut } from "redux/actions/SignQuizActions";
 
-interface propsType {
+interface QuizTimerProps {
   time: number;
 }
 
-const QuizTimer = ({ time }: propsType) => {
+const QuizTimer = ({ time }: QuizTimerProps) => {
   const dispatch = useDispatch();
   const expiryTimestamp = new Date();
   // eslint-disable-next-line @typescript-eslint/restrict-plus-operands
@@ -25,13 +25,25 @@ const QuizTimer = ({ time }: propsType) => {
     },
   });
 
+  const [barColor, setBarColor] = useState("green");
+
+  const calculateColor = (curTime: number, maxTime: number) => {
+    const green = Math.round((curTime / maxTime) * 255);
+    const red = 255 - green;
+    return `rgb(${red}, ${green}, 0)`;
+  };
+
+  useEffect(() => {
+    setBarColor(calculateColor(seconds - 1, time));
+  }, [seconds]);
+
   return (
     <div className="progressbar-container">
       <ProgressBar
         maxCompleted={time - 1}
         isLabelVisible={false}
         transitionTimingFunction="linear"
-        bgColor={"green"}
+        bgColor={barColor}
         completed={seconds - 1}
         labelAlignment="center"
         labelColor="#e80909"
