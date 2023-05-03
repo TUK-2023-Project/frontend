87cd7141eb876c8d5a6bc51f8b9ed66101c4e9da
minjilee <budlalswl1129@gmail.com>
2023-05-03 21:32:45 +0900

    hotfix: 폰트 크기, 색깔 스타일 수정

--- src/pages/Game/CategorySelection/index.tsx
+++ src/pages/Game/CategorySelection/index.tsx
@@ -30,6 +30,19 @@ const CategorySelection = () => {
         >
           {"선택이 완료되었다면 오른손을 활짝 펼쳐 카메라에 비춰주세요"}
         </h1>
+        <h1
+          className={
+            selectedButtonIndex !== null
+              ? styles["header__sub-title"]
+              : styles["header__sub-title"]
+          }
+          style={{
+            color: "red",
+            fontWeight: "bold",
+          }}
+        >
+          {"자음, 모음의 경우 오른손을 이용해 게임을 진행해주세요 (한 손)"}
+        </h1>
       </div>
 
       <div className={styles["category-wrapper"]}>
