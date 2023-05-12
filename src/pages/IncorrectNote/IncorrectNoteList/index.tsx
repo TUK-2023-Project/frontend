96036efb5afd2faca56f5bd3da50a7853b0f8e26
minjilee <budlalswl1129@gmail.com>
2023-05-12 22:10:40 +0900

    feat: 오답노트 리스트 api 연결

--- src/pages/IncorrectNote/IncorrectNoteList/index.tsx
+++ src/pages/IncorrectNote/IncorrectNoteList/index.tsx
@@ -2,56 +2,46 @@ import React, { useEffect, useState } from "react";
 import IncorrectNoteBox from "../components/ListCarousel";
 import styles from "./IncorrectNoteListPage.module.scss";
 import { Link } from "react-router-dom";
-
+import { getIncorrectListData } from "api/incorrectNote";
+import LoadingSpinner from "components/LoadingSpinner";
+interface incorrectData {
+  sign_id: number;
+  word: string;
+  wordtype: string;
+}
 function IncorrectNotePage() {
-  interface incorrectData {
-    word: string;
-    content: string;
-    img: string;
-  }
   const [consonant, setConsonant] = useState<incorrectData[]>();
   const [vowel, setVowel] = useState<incorrectData[]>();
-  const [alphabet, setAlphabet] = useState<incorrectData[]>();
-  const [number, setNumber] = useState<incorrectData[]>();
-  // 오답노트 리스트 가져오기
-  async function getIncorrectNoteData() {
-    try {
-      await fetch("../dummy/incorrectNote.json", {
-        headers: {
-          Accept: "application/json",
-        },
-        method: "GET",
-      })
-        .then((response: { json: () => any }) => response.json())
-        .then((result: any) => {
-          setConsonant(result.자음);
-          setVowel(result.모음);
-          setAlphabet(result.알파벳);
-          setNumber(result.숫자);
-        })
-        .catch((error: any) => {
-          console.log("error", error);
-        });
-    } catch (error) {
-      console.log(error);
-    }
-  }
-  useEffect(() => {
-    void getIncorrectNoteData();
-  }, []);
 
+  // 오답노트 리스트 가져오기
+  const { isLoading, error, data } = getIncorrectListData();
+
+  useEffect(() => {
+    const consonantList = [];
+    const vowelList = [];
+    for (let i = 0; i < data?.length; i++) {
+      if (data[i].wordtype === "1") {
+        consonantList.push(data[i]);
+      } else if (data[i].wordtype === "2") {
+        vowelList.push(data[i]);
+      }
+    }
+    setConsonant(consonantList);
+    setVowel(vowelList);
+  }, [data]);
+
+  if (isLoading) {
+    return <LoadingSpinner />;
+  }
   return (
     <div className={styles["incorrect-wrap"]}>
       <Link to="/main" className={styles["incorrect-wrap__home"]}>
         <img src="images/home.svg" alt="home" />
       </Link>
-
       <h1 className={styles["incorrect-wrap__title"]}>오답노트</h1>
       <div className={styles["incorrect-wrap__incorrectlist"]}>
-        <IncorrectNoteBox label="자음" data={consonant} />
-        <IncorrectNoteBox label="모음" data={vowel} />
-        <IncorrectNoteBox label="알파벳" data={alphabet} />
-        <IncorrectNoteBox label="숫자" data={number} />
+        <IncorrectNoteBox label="자음" item={consonant} />
+        <IncorrectNoteBox label="모음" item={vowel} />
       </div>
     </div>
   );
